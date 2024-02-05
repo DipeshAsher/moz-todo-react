aeed14f2d07cc1934bf15313e64b0a0d1536230c
Dipesh Asher <dipesh.asher1@nhs.net>
2024-02-05 22:24:52 +0000

    Added code to make TODO app start to look like an app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,112 @@
-import "./App.css";
-
-// const subject = "React";
-
 function App(props) {
-    console.log(props);
     return (
-        <>
-            <header>
-                {/* Hello, React :)! */}
-                <h1>Hello, {props.subject + " :)"}!</h1>
-                {/* Hello, REACT */}
-                <h1>Hello, {props.subject.toUpperCase()}</h1>
-                {/* Hello, 4 */}
-                <h1>Hello, {2 + 2}!</h1>
-                <button type="button" className="primary">
-                    Click me!
+        <div className="todoapp stack-large">
+            <h1>TodoMatic</h1>
+            <form>
+                <h2 className="label-wrapper">
+                    <label htmlFor="new-todo-input" className="label__lg">
+                        What needs to be done?
+                    </label>
+                </h2>
+                <input
+                    type="text"
+                    id="new-todo-input"
+                    className="input input__lg"
+                    name="text"
+                    autoComplete="off"
+                />
+                <button type="submit" className="btn btn__primary btn__lg">
+                    Add
                 </button>
-            </header>
-        </>
+            </form>
+            <div className="filters btn-group stack-exception">
+                <button
+                    type="button"
+                    className="btn toggle-btn"
+                    aria-pressed="true" // Tells assistive tech button is pressed
+                >
+                    <span className="visually-hidden">Show </span>
+                    <span>all</span>
+                    <span className="visually-hidden"> tasks</span>
+                    {/* visually-hidden providing assistive tech with additional info */}
+                </button>
+                <button
+                    type="button"
+                    className="btn toggle-btn"
+                    aria-pressed="false"
+                >
+                    <span className="visually-hidden">Show </span>
+                    <span>Active</span>
+                    <span className="visually-hidden"> tasks</span>
+                </button>
+                <button
+                    type="button"
+                    className="btn toggle-btn"
+                    aria-pressed="false"
+                >
+                    <span className="visually-hidden">Show </span>
+                    <span>Completed</span>
+                    <span className="visually-hidden"> tasks</span>
+                </button>
+            </div>
+            <h2 id="list-heading">3 tasks remaining</h2>
+            <ul
+                role="list" // Helps assistive tech explain what kind of element
+                className="todo-list stack-large stack-exception"
+                aria-labelledby="list-heading"  // That top row is column headings
+            >
+                <li className="todo stack-small">
+                    <div className="c-cb">
+                        <input id="todo-0" type="checkbox" defaultChecked />
+                        <label className="todo-label" htmlFor="todo-0">
+                            Eat
+                        </label>
+                    </div>
+                    <div className="btn-group">
+                        <button type="button" className="btn">
+                            Edit <span className="visually-hidden">Eat</span>
+                        </button>
+                        <button type="button" className="btn btn__danger">
+                            Delete <span className="visually-hidden">Eat</span>
+                        </button>
+                    </div>
+                </li>
+                <li className="todo stack-small">
+                    <div className="c-cb">
+                        <input id="todo-1" type="checkbox" />
+                        <label className="todo-label" htmlFor="todo-1">
+                            Sleep
+                        </label>
+                    </div>
+                    <div className="btn-group">
+                        <button type="button" className="btn">
+                            Edit <span className="visually-hidden">Sleep</span>
+                        </button>
+                        <button type="button" className="btn btn__danger">
+                            Delete{" "}
+                            <span className="visually-hidden">Sleep</span>
+                        </button>
+                    </div>
+                </li>
+                <li className="todo stack-small">
+                    <div className="c-cb">
+                        <input id="todo-2" type="checkbox" />
+                        <label className="todo-label" htmlFor="todo-2">
+                            Repeat
+                        </label>
+                    </div>
+                    <div className="btn-group">
+                        <button type="button" className="btn">
+                            Edit <span className="visually-hidden">Repeat</span>
+                        </button>
+                        <button type="button" className="btn btn__danger">
+                            Delete{" "}
+                            <span className="visually-hidden">Repeat</span>
+                        </button>
+                    </div>
+                </li>
+            </ul>
+        </div>
     );
 }
 
