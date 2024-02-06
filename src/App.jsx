53dbb18585a5c96169136c56522065bb0c9360ec
Dipesh Asher <dipesh.asher1@nhs.net>
2024-02-06 15:24:43 +0000

    Componentised Todo as separate module and component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import Todo from "./components/Todo";
+
 function App(props) {
+    const taskList = props.tasks?.map((task) => (
+        <Todo
+            id={task.id}
+            name={task.name}
+            completed={task.completed}
+            key={task.id}
+        />
+    ));
+
     return (
         <div className="todoapp stack-large">
             <h1>TodoMatic</h1>
@@ -53,58 +64,9 @@ function App(props) {
             <ul
                 role="list" // Helps assistive tech explain what kind of element
                 className="todo-list stack-large stack-exception"
-                aria-labelledby="list-heading"  // That top row is column headings
+                aria-labelledby="list-heading" // That top row is column headings
             >
-                <li className="todo stack-small">
-                    <div className="c-cb">
-                        <input id="todo-0" type="checkbox" defaultChecked />
-                        <label className="todo-label" htmlFor="todo-0">
-                            Eat
-                        </label>
-                    </div>
-                    <div className="btn-group">
-                        <button type="button" className="btn">
-                            Edit <span className="visually-hidden">Eat</span>
-                        </button>
-                        <button type="button" className="btn btn__danger">
-                            Delete <span className="visually-hidden">Eat</span>
-                        </button>
-                    </div>
-                </li>
-                <li className="todo stack-small">
-                    <div className="c-cb">
-                        <input id="todo-1" type="checkbox" />
-                        <label className="todo-label" htmlFor="todo-1">
-                            Sleep
-                        </label>
-                    </div>
-                    <div className="btn-group">
-                        <button type="button" className="btn">
-                            Edit <span className="visually-hidden">Sleep</span>
-                        </button>
-                        <button type="button" className="btn btn__danger">
-                            Delete{" "}
-                            <span className="visually-hidden">Sleep</span>
-                        </button>
-                    </div>
-                </li>
-                <li className="todo stack-small">
-                    <div className="c-cb">
-                        <input id="todo-2" type="checkbox" />
-                        <label className="todo-label" htmlFor="todo-2">
-                            Repeat
-                        </label>
-                    </div>
-                    <div className="btn-group">
-                        <button type="button" className="btn">
-                            Edit <span className="visually-hidden">Repeat</span>
-                        </button>
-                        <button type="button" className="btn btn__danger">
-                            Delete{" "}
-                            <span className="visually-hidden">Repeat</span>
-                        </button>
-                    </div>
-                </li>
+                {taskList}
             </ul>
         </div>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App subject="Kevin" />
+    <App tasks={DATA} />
+    {/* {console.log(DATA)} */}
   </React.StrictMode>,
 )
