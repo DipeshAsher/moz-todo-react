0f59a62c0eb0ed8c734f6579acaa66ccbbc6022d
Dipesh Asher <dipesh.asher1@nhs.net>
2024-02-07 11:37:50 +0000

    Added events and handling state and implemented functionality to add tasks, delete tasks and toggle tasks as completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,65 @@
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App(props) {
-    const taskList = props.tasks?.map((task) => (
+    
+    function addTask(name) {
+        const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+        setTasks([...tasks, newTask]);
+    }
+    
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map((task) => {
+            // if this task has the same ID as the edited task
+            if (id === task.id) {
+                // use object spread to make a new object
+                // whose `completed` prop has been inverted
+                return { ...task, completed: !task.completed };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+        console.log(updatedTasks);
+    }
+    
+    function deleteTask(id) {
+        const remainingTasks = tasks.filter((task) => id !== task.id);
+        setTasks(remainingTasks);
+        console.log(remainingTasks);
+    }   
+    
+    const [tasks, setTasks] = useState(props.tasks);
+    const taskList = tasks?.map((task) => (
         <Todo
             id={task.id}
             name={task.name}
             completed={task.completed}
             key={task.id}
+            toggleTaskCompleted={toggleTaskCompleted}
+            deleteTask={deleteTask}
         />
     ));
+
+    const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+    const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
     return (
         <div className="todoapp stack-large">
             <h1>TodoMatic</h1>
-            <form>
-                <h2 className="label-wrapper">
-                    <label htmlFor="new-todo-input" className="label__lg">
-                        What needs to be done?
-                    </label>
-                </h2>
-                <input
-                    type="text"
-                    id="new-todo-input"
-                    className="input input__lg"
-                    name="text"
-                    autoComplete="off"
-                />
-                <button type="submit" className="btn btn__primary btn__lg">
-                    Add
-                </button>
-            </form>
+            <Form addTask={addTask} />
             <div className="filters btn-group stack-exception">
-                <button
-                    type="button"
-                    className="btn toggle-btn"
-                    aria-pressed="true" // Tells assistive tech button is pressed
-                >
-                    <span className="visually-hidden">Show </span>
-                    <span>all</span>
-                    <span className="visually-hidden"> tasks</span>
-                    {/* visually-hidden providing assistive tech with additional info */}
-                </button>
-                <button
-                    type="button"
-                    className="btn toggle-btn"
-                    aria-pressed="false"
-                >
-                    <span className="visually-hidden">Show </span>
-                    <span>Active</span>
-                    <span className="visually-hidden"> tasks</span>
-                </button>
-                <button
-                    type="button"
-                    className="btn toggle-btn"
-                    aria-pressed="false"
-                >
-                    <span className="visually-hidden">Show </span>
-                    <span>Completed</span>
-                    <span className="visually-hidden"> tasks</span>
-                </button>
+                <FilterButton />
+                <FilterButton />
+                <FilterButton />
             </div>
-            <h2 id="list-heading">3 tasks remaining</h2>
+            <h2 id="list-heading">{headingText}</h2>
             <ul
-                role="list" // Helps assistive tech explain what kind of element
+                role="list"
                 className="todo-list stack-large stack-exception"
-                aria-labelledby="list-heading" // That top row is column headings
+                aria-labelledby="list-heading"
             >
                 {taskList}
             </ul>
